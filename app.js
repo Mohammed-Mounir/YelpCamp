2a0a439e87b9b9058f45fd36e144545a9a050796
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2021-10-14 10:18:43 +0200

    Setting local server

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(3000, () => {
+  console.log("Servering on port 3000");
+});

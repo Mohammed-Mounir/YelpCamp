f7e77ed44f189698d16f31a396ea78b98973433e
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2021-10-20 12:47:52 +0200

    Added basic styles

--- models/campground.js
+++ models/campground.js
@@ -1,11 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
   title: String,
-  price: String,
+  image: String,
+  price: Number,
   description: String,
   location: String,
 });
 
-module.exports = mongoose.model("Campground", CampgroundSchema);
+module.exports = mongoose.model('Campground', CampgroundSchema);
--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt molestias expedita, quasi aliquam, aspernatur sint temporibus ad deleniti nisi beatae cumque commodi, tempore doloremque distinctio sunt sapiente doloribus modi illo!',
+      price,
     });
     await camp.save();
   }
